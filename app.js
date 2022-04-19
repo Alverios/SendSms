949c85132483455f2e1bb96bac3af9e1cc2761d2
Alvin Alinda <aalinda@local.internal>
2022-04-19 16:32:01 +0300

    New and First commit!

---
+++ app.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const socketio = require('socket.io');
+const africastalking = require('africastalking');
+
+// Init africastalking
+const AfricasTalking = new africastalking({
+  apiKey: '12622bb584a11e6a6704cbaca0f18e5921c395792678ff8a08fbcff8296c14c0',
+  username: 'wagwansms'
+}, {debug: true});
+
+// Init app
+const app = express();
+
+// Template engine setup
+app.set('view engine', 'html');
+app.engine('html', ejs.renderFile);
+
+// Public folder setup
+app.use(express.static(__dirname + '/public'));
+
+// Body parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Index route
+app.get('/', (req, res) => {
+  res.render('index');
+});
+// Catch form submit
+app.post('/', (req, res) => {
+  // res.send(req.body);
+  // console.log(req.body);
+  const to = req.body.number;
+  const message = req.body.text;
+  const sms = AfricasTalking.SMS;
+     sms.send({to: `+256${to}`, message})
+    .then(success => console.log({message: message, to: to}))
+    .catch(error => console.log(error));
+    // Get data from response
+    const data = {
+      message: message,
+      number: to
+    }
+
+    // Emit to the client
+    io.emit('smsStatus', data);
+
+});
+
+// Define port
+const port = 5000;
+
+// Start server
+const server = app.listen(port, () => console.log(`We Love Nerds ${port}`));
+
+// Connect to socket.io
+const io = socketio(server);
+io.on('connection', (socket) => {
+  console.log('Connected');
+  io.on('disconnect', () => {
+    console.log('Disconnected');
+  })
+})
+
